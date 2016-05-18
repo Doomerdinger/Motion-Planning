402c818f82264d435d903b9cca94a5eefbb6fa04
Alec Tiefenthal <alectief@gmail.com>
2016-05-18 13:32:46 -0400

    Changed gitignore. Working on node representation for A*.

---
+++ Alec Unreal/Top Down/TopDown/Source/TopDown/MyNavigationData.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AI/Navigation/NavigationData.h"
+#include "MyNavigationData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API AMyNavigationData : public ANavigationData
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
