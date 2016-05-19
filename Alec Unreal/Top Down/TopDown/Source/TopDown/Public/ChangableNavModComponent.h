4568d32b1d8473758a58a1ffd42c7e8603fa6446
Alec Tiefenthal <alectief@gmail.com>
2016-05-19 07:30:35 -0400

    Not working as intended, sadly. Looks like it is not possible without a substantial additional amount of knowledge about Unreal.

---
+++ Alec Unreal/Top Down/TopDown/Source/TopDown/Public/ChangableNavModComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AI/Navigation/NavModifierComponent.h"
+#include "ChangableNavModComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_API UChangableNavModComponent : public UNavModifierComponent
+{
+	GENERATED_BODY()
+	
+	//UPROPERTY(EditAnywhere, Category = Navigation)
+	//TSubclassOf<UNavArea> AreaClass;
+	
+	
+};
